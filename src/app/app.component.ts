c74883525d3143505ce456b888256945ebdf99f9
Stefan Jaroni <sjaroni@outlook.de>
2024-04-02 23:57:36 +0200

    feat: add first routes, add new component imprint and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { LogInComponent } from './main-content/log-in/log-in.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    LogInComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dabubble';
+  isOnline: boolean = false; // muss aus dem Service kommen
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  { path: '', component: AppComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+
+  // { path: '', component: ComponentName },
+  // { path: '', component: ComponentName },
+  // { path: '', component: ComponentName },
+  // { path: '', component: ComponentName },
+
+];
