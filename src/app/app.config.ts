1c3b8904e769b67c299ca6cdc0eeb73944337ab5
Stefan Jaroni <sjaroni@outlook.de>
2024-04-02 22:52:06 +0200

    feat: add firebaseConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { firebaseConfig } from './environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      provideFirebaseApp(() => 
+      initializeApp(
+        {
+          "projectId": firebaseConfig.projectId,
+          "appId": firebaseConfig.appId,
+          "storageBucket":firebaseConfig.storageBucket,
+          "apiKey": firebaseConfig.apiKey,
+          "authDomain":firebaseConfig.authDomain,
+          "messagingSenderId":firebaseConfig.messagingSenderId,
+        }
+      )
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage()))]
 };
