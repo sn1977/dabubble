356efa8942d89f9eb8744639606fef985e6f87f3
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-04-03 16:10:36 +0200

    routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { ImprintComponent } from './imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { MessagerComponent } from './messager/messager.component';
+import { ChannelComponent } from './channel/channel/channel.component';
 
 export const routes: Routes = [  
   { path: '', component: MainContentComponent },
   { path: 'imprint', component: ImprintComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'messager', component: MessagerComponent}
+  { path: 'channel', component: ChannelComponent}
 
   // { path: '', component: ComponentName },
   // { path: '', component: ComponentName },
