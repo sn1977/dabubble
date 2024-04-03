a9b7f09e29e40290d4f416a85e50273b52c2d331
Stefan Jaroni <sjaroni@outlook.de>
2024-04-03 10:29:27 +0200

    style: add mixins, improve logi-in component

--- src/app/main-content/log-in/log-in.component.ts
+++ src/app/main-content/log-in/log-in.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatDialogModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
-
+  user: string | undefined = '';
 }
