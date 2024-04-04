e09f36a0960593a8aca52b428fdef3059c87e7dd
Stefan Jaroni <sjaroni@outlook.de>
2024-04-04 16:42:51 +0200

    feat: add authentication with email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LogInComponent } from './main-content/log-in/log-in.component';
+import {HeaderMobileComponent} from './shared/components/header-mobile/header-mobile.component';
+import {MainContentComponent} from './main-content/main-content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
