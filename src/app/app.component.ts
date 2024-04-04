9d2db38bcd52edbff90f6fd0404b186bb3e9fa82
Sascha Nemeth <sascha.nemeth@me.com>
2024-04-04 14:56:09 +0200

    mobile-header & main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LogInComponent } from './main-content/log-in/log-in.component';
+import {HeaderMobileComponent} from './shared/components/header-mobile/header-mobile.component';
+import {MainContentComponent} from './main-content/main-content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
+    CommonModule,
     RouterOutlet,
-    LogInComponent
+    LogInComponent,
+    HeaderMobileComponent,
+    MainContentComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
