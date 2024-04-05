0202be686bbf1c4805fbc00bec3ecba62f9f8fa4
Stefan Jaroni <sjaroni@outlook.de>
2024-04-05 22:20:50 +0200

    testing authguard.service

--- src/app/shared/services/authguard.service.ts
+++ src/app/shared/services/authguard.service.ts
@@ -3,12 +3,10 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router,
-  CanActivate,
-  UrlTree,
   CanActivateFn,
 } from '@angular/router';
-import { Observable, catchError, map } from 'rxjs';
 import { AuthService } from './auth.service';
+import { getAuth } from '@angular/fire/auth';
 
 export
 @Injectable({
@@ -23,18 +21,18 @@ class AuthGuardService {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): any {    
+  ): boolean {    
     
     //https://stackoverflow.com/questions/77844746/angular-how-to-resolve-canactivate-deprecated-in-angular-15-using-auth-guard
 
-    this.authService.user$.subscribe((user) => {
-      if (user) {
-        return true;
-      } else {
-        return false;
-      }
-      //console.log('Current User', this.authService.currentUserSig());
-    })
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
     
     // if (this.authService.currentUserSig()?.username != null) {
     //   console.log(this.authService.currentUserSig()?.username);      
