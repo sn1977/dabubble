64cabfe30529ec07e351e41240a44e3ab8d2943c
Sascha Nemeth <sascha.nemeth@me.com>
2024-04-05 15:50:07 +0200

    adding bottom sheet

---
+++ src/app/main-content/bottom-sheet/bottom-sheet.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {MatNavList} from '@angular/material/list';
+import {MatBottomSheetModule, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatLine} from '@angular/material/core';
+
+
+@Component({
+  selector: 'app-bottom-sheet',
+  standalone: true,
+  imports: [
+    MatNavList,
+    MatBottomSheetModule,
+    MatListModule,
+    MatButtonModule,
+    MatLine
+  ],
+  templateUrl: './bottom-sheet.component.html',
+  styleUrl: './bottom-sheet.component.scss'
+})
+export class BottomSheetComponent {
+  constructor(private _bottomSheetRef: MatBottomSheetRef<BottomSheetComponent>) {}
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
