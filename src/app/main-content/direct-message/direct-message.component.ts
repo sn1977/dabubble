2307f94afa3d35267ae566f2432f3bc80a200bec
Sascha Nemeth <sascha.nemeth@me.com>
2024-04-08 21:53:04 +0200

    add direct-message-overlay-card and connnected with direct-message

--- src/app/main-content/direct-message/direct-message.component.ts
+++ src/app/main-content/direct-message/direct-message.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DirectMessageOverlayComponent} from '../direct-message-overlay/direct-message-overlay.component';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-direct-message',
@@ -8,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrl: './direct-message.component.scss'
 })
 export class DirectMessageComponent {
+  // constructor(public dialogRef: MatDialogRef<DirectMessageOverlayComponent>, public dialog: MatDialog) {}
+  //
+  // @Inject(MAT_DIALOG_DATA) public data: any
+  //
+  // onNoClick(): void {
+  //   this.dialogRef.close();
+  // }
+  //
+  // openDirectMessageOverlay(): void {
+  //   const dialogRef = this.dialog.open(DirectMessageOverlayComponent, {
+  //     minWidth: '398px',
+  //     minHeight: '600px',
+  //     panelClass: 'custom-dialog-container',
+  //     // Weitere Konfigurationen nach Bedarf
+  //   });
+  //   // this.closeProfilCard();
+  // }
 
+  constructor(public dialog: MatDialog) {}
+
+  openDirectMessageOverlay(): void {
+    const dialogRef = this.dialog.open(DirectMessageOverlayComponent, {
+      minWidth: '398px',
+      minHeight: '600px',
+      panelClass: 'custom-dialog-container',
+      // Weitere Konfigurationen nach Bedarf
+    });
+    // this.closeProfilCard();
+  }
 }
