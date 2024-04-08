e47d4280e108986b12c123eea9826202d7d88c42
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-04-08 11:12:30 +0200

    new channel

--- src/app/new-channel/new-channel.component.ts
+++ src/app/new-channel/new-channel.component.ts
@@ -9,22 +9,25 @@ import { Component } from '@angular/core';
 })
 export class NewChannelComponent {
 
-  toggleOverlay(overlayId: string): void {
-    const overlay = document.getElementById(overlayId);
-    if (overlay) {
-        if (overlay.style.display === "block") {
-            overlay.style.display = "none";
-        } else {
-            overlay.style.display = "block";
+    toggleOverlay(overlayId: string): void {
+        const currentOverlay = document.querySelector('.overlay[style="display: block;"]') as HTMLElement;
+        const newOverlay = document.getElementById(overlayId);
+      
+        if (currentOverlay && currentOverlay.id !== overlayId) {
+          // Schließe das aktuelle Overlay, wenn ein anderes Overlay geöffnet ist
+          currentOverlay.style.display = "none";
         }
-    }
-}
-
-closeOverlay(overlayId: string): void {
-    const overlay = document.getElementById(overlayId);
-    if (overlay && overlay.style.display === "block") {
-        overlay.style.display = "none";
-    }
-}
+      
+        if (newOverlay) {
+          newOverlay.style.display = newOverlay.style.display === "none" ? "block" : "none";
+        }
+      }
+      
+      closeOverlay(overlayId: string): void {
+        const overlay = document.getElementById(overlayId) as HTMLElement;
+        if (overlay) {
+          overlay.style.display = "none";
+        }
+      }
 }
 
