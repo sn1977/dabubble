7725f1fd19a9961b3e76ffe89f03bad6c69f2d3f
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-04-09 13:10:08 +0200

    add-channel

--- src/app/main-content/add-channel/add-channel.component.ts
+++ src/app/main-content/add-channel/add-channel.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-add-channel',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './add-channel.component.html',
   styleUrl: './add-channel.component.scss'
 })
 export class AddChannelComponent {
+  
+    overlayVisible: boolean = false;
+    showInputField: boolean = false;
+
+    toggleOverlay() {
+        this.overlayVisible = !this.overlayVisible;
+    }
+
+    toggleInputField(event: any) {
+        // Überprüfen Sie, welche Checkbox ausgewählt ist und aktualisieren Sie entsprechend die Sichtbarkeit des Input-Felds
+        this.showInputField = event.target.id === 'addSpecificMembers' && event.target.checked;
+    }
+
+
+
 
 }
