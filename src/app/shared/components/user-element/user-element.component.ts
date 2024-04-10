92a3de1f5ee394fb17bb52d8f18f74238801688f
Stefan Jaroni <sjaroni@outlook.de>
2024-04-10 17:34:09 +0200

    testing app-user-element

--- src/app/shared/components/user-element/user-element.component.ts
+++ src/app/shared/components/user-element/user-element.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-element',
   standalone: true,
   imports: [],
   templateUrl: './user-element.component.html',
-  styleUrl: './user-element.component.scss'
+  styleUrl: './user-element.component.scss',
 })
 export class UserElementComponent {
-  @Input()user = {
-    id: 0,    
+  @Input() user = {
+    id: '',
+    isOnline: false,
+    name: '',
+    avatar: '',
   };
 }
