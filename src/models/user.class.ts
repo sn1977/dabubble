8ff87d04eecd3fb1f3115344047b9d4de55900ce
Stefan Jaroni <sjaroni@outlook.de>
2024-04-15 20:31:05 +0200

    style: improve animation smoothness

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,14 +1,14 @@
 export class User {
   id?: string;
-  avatar: string;  
-  email: string;  
+  avatar?: string;
+  email: string;
   name: string;
   isOnline: boolean;
   provider: string;
 
   constructor(obj?: any) {
-    this.avatar = obj ? obj.avatar : '';    
-    this.email = obj ? obj.email : '';    
+    // this.avatar = obj ? obj.avatar : '';
+    this.email = obj ? obj.email : '';
     this.name = obj ? obj.name : '';
     this.isOnline = obj ? obj.isOnline : true;
     this.provider = obj ? obj.provider : '';
@@ -17,7 +17,7 @@ export class User {
   public toJSON() {
     return {
       avatar: this.avatar,
-      email: this.email,      
+      email: this.email,
       name: this.name,
       isOnline: this.isOnline,
       provider: this.provider,
