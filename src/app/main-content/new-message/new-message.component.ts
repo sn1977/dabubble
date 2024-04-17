3cd1341257ac80a0ae7dd70ccbf27473fba2a68e
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-04-17 23:01:25 +0200

    header in new message

--- src/app/main-content/new-message/new-message.component.ts
+++ src/app/main-content/new-message/new-message.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { BottomSheetComponent } from '../bottom-sheet/bottom-sheet.component';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
+import { Auth } from '@angular/fire/auth';
+import { FirebaseService } from '../../shared/services/firebase.service';
+import { User } from '../../../models/user.class';
 
 @Component({
   selector: 'app-new-message',
@@ -16,8 +20,33 @@ export class NewMessageComponent {
   constructor(private _bottomSheet: MatBottomSheet){
     
   }
-
+  firebaseAuth = inject(Auth);
+  authService = inject(AuthService);
+  firestore = inject(FirebaseService);
   router = inject(Router);
+  user: User = new User();
+
+  async ngOnInit(): Promise<void> {
+    await this.waitForUserData();
+    this.test();
+
+  }
+
+  async waitForUserData(): Promise<void> {
+    while (!this.authService.activeUserAccount) {
+      await this.delay(100); // Wartezeit in Millisekunden, bevor erneut überprüft wird
+    }
+  }
+
+  delay(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  test() {
+    let id = this.authService.activeUserAccount.uid;
+    console.log(id); // Stelle sicher, dass id definiert ist, bevor du darauf zugreifst
+    this.getItemValuesProfile('users', id);
+  }
   
   toggleOverlay(overlayId: string): void {
     const currentOverlay = document.querySelector('.overlay[style="display: block;"]') as HTMLElement;
@@ -42,6 +71,13 @@ export class NewMessageComponent {
 
   openBottomSheet(): void {
     this._bottomSheet.open(BottomSheetComponent);
+  }
+
+  getItemValuesProfile(collection: string, itemID: string) {
+    this.firestore.getSingleItemData(collection, itemID, () => {
+      this.user = new User(this.firestore.user);
+      console.log('Avatar: ' + this.user.avatar);
+    });
   }
 
 }
