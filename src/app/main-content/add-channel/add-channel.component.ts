3bb88ed88df1c4cee7ab09e8c3092bafd4201c8d
yannick.vaterlaus@students.ffhs.ch <yannick.vaterlaus@windowslive.com>
2024-04-18 10:39:34 +0200

    add-channel

--- src/app/main-content/add-channel/add-channel.component.ts
+++ src/app/main-content/add-channel/add-channel.component.ts
@@ -3,41 +3,50 @@ import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { FirebaseService } from '../../shared/services/firebase.service';
-
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-channel',
   standalone: true,
-  imports: [NgIf, FormsModule, RouterLink],
+  imports: [NgIf, FormsModule, RouterLink, CommonModule],
   templateUrl: './add-channel.component.html',
-  styleUrl: './add-channel.component.scss'
+  styleUrl: './add-channel.component.scss',
 })
-export class AddChannelComponent {
+export class AddChannelComponent  {
+  users: string[] = ['Max', 'Peter', 'Anna', 'Lisa'];
+  filteredUsers: string[] = [];
+  showDropdown: boolean = false;
+  firestore = inject(FirebaseService);
+  router = inject(Router);
 
-    
+  overlayVisible: boolean = false;
+  showInputField: boolean = false;
 
-    showDropdown: boolean = false;
-    firestore = inject(FirebaseService);
-    router = inject(Router);
-  
-    overlayVisible: boolean = false;
-    showInputField: boolean = false;
-
-    toggleOverlay() {
-        this.overlayVisible = !this.overlayVisible;
+  filterUsers(event: any): void {
+    const searchText: string = event.target.value;
+    if (!searchText) {
+      this.filteredUsers = [];
+      return;
     }
+    this.filteredUsers = this.users.filter((user) =>
+      user.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
 
-    toggleInputField(event: any) {
-        // Überprüfen Sie, welche Checkbox ausgewählt ist und aktualisieren Sie entsprechend die Sichtbarkeit des Input-Felds
-        this.showInputField = event.target.id === 'addSpecificMembers' && event.target.checked;
-    }
+  toggleOverlay() {
+    this.overlayVisible = !this.overlayVisible;
+  }
 
-    toggleDropdown() {
-        this.showDropdown = !this.showDropdown;
-      }
+  toggleInputField(event: any) {
+    this.showInputField =
+      event.target.id === 'addSpecificMembers' && event.target.checked;
+  }
 
+  getInputValue(event: any): string {
+    return event && event.target && event.target.value;
+  }
 
-
-
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
 }
