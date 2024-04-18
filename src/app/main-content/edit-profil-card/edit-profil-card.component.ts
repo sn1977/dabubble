c53eea2b19c7bafaa68d215aa7dee3bee58fd05e
Sascha Nemeth <sascha.nemeth@me.com>
2024-04-18 20:40:54 +0200

    add save of edit-profil-card for logged in user correction

--- src/app/main-content/edit-profil-card/edit-profil-card.component.ts
+++ src/app/main-content/edit-profil-card/edit-profil-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {Component, inject, Inject, OnDestroy, OnInit} from '@angular/core';
 import {MatCard, MatCardActions, MatCardContent, MatCardImage} from '@angular/material/card';
 import {NgOptimizedImage} from '@angular/common';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
@@ -6,6 +6,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButton} from '@angular/material/button';
 import {User} from '../../../models/user.class';
 import {FirebaseService} from '../../shared/services/firebase.service';
+import {AuthService} from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-edit-profil-card',
@@ -24,6 +25,7 @@ import {FirebaseService} from '../../shared/services/firebase.service';
   styleUrl: './edit-profil-card.component.scss'
 })
 export class EditProfilCardComponent implements OnInit, OnDestroy {
+  authService = inject(AuthService);
   user: User = new User();
 
   nameData = {
@@ -70,9 +72,10 @@ export class EditProfilCardComponent implements OnInit, OnDestroy {
     this.data.user.displayName = this.nameData.name;
     this.data.user.email = this.emailData.email;
 
-    this.firebaseService.updateUser(this.data.user, '9MacQRd4i2TX9J42mVLBGgVCsPp1').then(() => {
-      console.log('Profil aktualisiert');
-      console.log(this.data.user);
+    this.firebaseService.updateUser(this.data.user, this.authService.activeUserAccount.uid).then(() => {
+      // console.log('Profil aktualisiert');
+      // console.log(this.authService.activeUserAccount.uid);
+      // console.log(this.data.user);
       this.dialogRef.close();
     }).catch(error => {
       console.error('Fehler beim Aktualisieren des Profils:', error);
