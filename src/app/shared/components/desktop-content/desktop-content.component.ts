d1496220e175795c07a7b4ce4fbecdbb92027b33
Stefan Jaroni <sjaroni@outlook.de>
2024-05-03 23:30:24 +0200

    style: add material-cards on desktop

--- src/app/shared/components/desktop-content/desktop-content.component.ts
+++ src/app/shared/components/desktop-content/desktop-content.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { MainContentComponent } from "../../../main-content/main-content.component";
 import { CommonModule } from '@angular/common';
-
+import {MatCardModule} from '@angular/material/card';
 @Component({
     selector: 'app-desktop-content',
     standalone: true,
     templateUrl: './desktop-content.component.html',
     styleUrl: './desktop-content.component.scss',
-    imports: [MainContentComponent, CommonModule]
+    imports: [MainContentComponent, CommonModule,MatCardModule]
 })
 export class DesktopContentComponent {
 
