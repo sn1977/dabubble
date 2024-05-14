354e9839be71d64f0b849848c89df0d8913f9ca4
Stefan Jaroni <sjaroni@outlook.de>
2024-05-14 07:16:40 +0200

    improve code

--- src/app/shared/components/desktop-content/desktop-content.component.ts
+++ src/app/shared/components/desktop-content/desktop-content.component.ts
@@ -1,22 +1,13 @@
-import {Component, Input} from '@angular/core';
-import { MainContentComponent } from "../../../main-content/main-content.component";
+import { Component, Input } from '@angular/core';
+import { MainContentComponent } from '../../../main-content/main-content.component';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
-// import {ConversationComponent} from '../conversation/conversation.component';
-// import {NewChannelComponent} from '../../../main-content/new-channel/new-channel.component';
-// import {NewMessageComponent} from '../../../main-content/new-message/new-message.component';
+import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
 @Component({
-    selector: 'app-desktop-content',
-    standalone: true,
-    templateUrl: './desktop-content.component.html',
-    styleUrl: './desktop-content.component.scss',
-  imports: [
-    MainContentComponent, 
-    CommonModule, 
-    MatCardModule,
-    RouterOutlet ]
+  selector: 'app-desktop-content',
+  standalone: true,
+  templateUrl: './desktop-content.component.html',
+  styleUrl: './desktop-content.component.scss',
+  imports: [MainContentComponent, CommonModule, MatCardModule, RouterOutlet],
 })
-export class DesktopContentComponent {
-
-}
+export class DesktopContentComponent {}
