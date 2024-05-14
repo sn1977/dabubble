15c0fcee0644e3b8f8341326e1cc9597b64da2d8
Stefan Jaroni <sjaroni@outlook.de>
2024-05-14 07:11:22 +0200

    feat: add router-outlet

--- src/app/shared/components/desktop-content/desktop-content.component.ts
+++ src/app/shared/components/desktop-content/desktop-content.component.ts
@@ -2,15 +2,20 @@ import {Component, Input} from '@angular/core';
 import { MainContentComponent } from "../../../main-content/main-content.component";
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
-import {ConversationComponent} from '../conversation/conversation.component';
-import {NewChannelComponent} from '../../../main-content/new-channel/new-channel.component';
-import {NewMessageComponent} from '../../../main-content/new-message/new-message.component';
+// import {ConversationComponent} from '../conversation/conversation.component';
+// import {NewChannelComponent} from '../../../main-content/new-channel/new-channel.component';
+// import {NewMessageComponent} from '../../../main-content/new-message/new-message.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-desktop-content',
     standalone: true,
     templateUrl: './desktop-content.component.html',
     styleUrl: './desktop-content.component.scss',
-  imports: [MainContentComponent, CommonModule, MatCardModule, ConversationComponent, NewChannelComponent, NewMessageComponent]
+  imports: [
+    MainContentComponent, 
+    CommonModule, 
+    MatCardModule,
+    RouterOutlet ]
 })
 export class DesktopContentComponent {
 
