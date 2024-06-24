f88c963c3ab6d6243e97d95536d8166364ee1940
Stefan Jaroni <sjaroni@outlook.de>
2024-06-24 21:38:34 +0200

    refactor: remove unused code and comments

--- src/models/channel.class.ts
+++ src/models/channel.class.ts
@@ -3,15 +3,13 @@ export class Channel {
   creator: string;
   description: string;
   member: any[];
-  name: string;  
-  // newMessage: boolean | undefined;
+  name: string;
 
   constructor(obj?: any) {
     this.creator = obj ? obj.creator : '';
     this.description = obj ? obj.description : '';
     this.member = obj && obj.member ? obj.member : [];
     this.name = obj ? obj.name : '';
-    // this.newMessage= obj ? obj.newMessage : false;
   }
 
   public toJSON() {
@@ -20,7 +18,6 @@ export class Channel {
       description: this.description,
       member: this.member,
       name: this.name,
-      // newMessage: this.newMessage,
     };
   }
 }
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -4,20 +4,14 @@ export class User {
   email: string;
   displayName: string | null;
   isOnline: boolean;
-  // newMessage: boolean | undefined;
   provider: string;
-  // selected: boolean | undefined;
-  // count: number;
 
   constructor(obj?: any) {
     this.avatar = obj ? obj.avatar : '';
     this.email = obj ? obj.email : '';
     this.displayName = obj ? obj.displayName : '';
-    this.isOnline = obj ? obj.isOnline : true;
-    // this.newMessage= obj ? obj.newMessage : false;
+    this.isOnline = obj ? obj.isOnline : true;    
     this.provider = obj ? obj.provider : '';
-    // this.selected= obj ? obj.selected : false;
-    // this.count = obj ? obj.count: 0;
   }
 
   public toJSON() {
@@ -26,10 +20,7 @@ export class User {
       email: this.email,
       displayName: this.displayName,
       isOnline: this.isOnline,
-      // newMessage: this.newMessage,
       provider: this.provider,
-      // selected: this.selected,
-      // count: this.count,
     };
   }
 }
