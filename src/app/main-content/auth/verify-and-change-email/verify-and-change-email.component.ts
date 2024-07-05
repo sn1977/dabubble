d82c531494cb67294b70c83da858b7e605e058a3
Stefan Jaroni <sjaroni@outlook.de>
2024-07-05 12:47:04 +0200

    feat: add loading-animation

--- src/app/main-content/auth/verify-and-change-email/verify-and-change-email.component.ts
+++ src/app/main-content/auth/verify-and-change-email/verify-and-change-email.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth.service';
+import { ProgressSpinnerComponent } from "../../../shared/components/progress-spinner/progress-spinner.component";
 
 @Component({
-  selector: 'app-verify-and-change-email',
-  standalone: true,
-  imports: [],
-  templateUrl: './verify-and-change-email.component.html',
-  styleUrl: './verify-and-change-email.component.scss',
+    selector: 'app-verify-and-change-email',
+    standalone: true,
+    templateUrl: './verify-and-change-email.component.html',
+    styleUrl: './verify-and-change-email.component.scss',
+    imports: [ProgressSpinnerComponent]
 })
 export class VerifyAndChangeEmailComponent implements OnInit {
   authService = inject(AuthService);
