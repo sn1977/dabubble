33d305d653e7f5fe9d4e4ee18fb71ea060052fa2
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-08 20:34:31 +0200

    refactor: Enable noUnusedLocals and noUnusedParameters in tsconfig.json ``

--- src/app/shared/components/desktop-content/desktop-content.component.ts
+++ src/app/shared/components/desktop-content/desktop-content.component.ts
@@ -1,49 +1,64 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { MainContentComponent } from '../../../main-content/main-content.component';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from "@angular/core";
+import { MainContentComponent } from "../../../main-content/main-content.component";
+import { CommonModule } from "@angular/common";
+import { MatCardModule } from "@angular/material/card";
+import { RouterModule, RouterOutlet } from "@angular/router";
 import { ThreadComponent } from "../thread/thread.component";
-import { MatchMediaService } from '../../services/match-media.service';
-import { Router, RouterLink } from '@angular/router';
+import { MatchMediaService } from "../../services/match-media.service";
+import { Router, RouterLink } from "@angular/router";
 @Component({
-    selector: 'app-desktop-content',
+    selector: "app-desktop-content",
     standalone: true,
-    templateUrl: './desktop-content.component.html',
-    styleUrl: './desktop-content.component.scss',
+    templateUrl: "./desktop-content.component.html",
+    styleUrl: "./desktop-content.component.scss",
     imports: [
-      MainContentComponent, 
-      CommonModule, 
-      MatCardModule, 
-      RouterOutlet,
-      RouterLink,
-      RouterModule, 
-      ThreadComponent
-    ]
+        MainContentComponent,
+        CommonModule,
+        MatCardModule,
+        RouterOutlet,
+        RouterLink,
+        RouterModule,
+        ThreadComponent,
+    ],
 })
 export class DesktopContentComponent implements OnInit {
-  matchMedia = inject(MatchMediaService);
-  router = inject(Router);
-  isDesktop: boolean = false;
-  isCollapsed: boolean = false;
-  hovered: boolean = false;
+    matchMedia = inject(MatchMediaService);
+    router = inject(Router);
+    isDesktop: boolean = false;
+    isCollapsed: boolean = false;
+    hovered: boolean = false;
 
-  ngOnInit(): void {
-    this.isDesktop = this.matchMedia.checkIsDesktop();
-  }
+    /**
+     * Initializes the component.
+     * This method is called after the component has been created and initialized.
+     */
+    ngOnInit(): void {
+        this.isDesktop = this.matchMedia.checkIsDesktop();
+    }
 
-  toggleDiv() {    
-    this.matchMedia.showSearchDropdown = false;
-    this.isCollapsed = !this.isCollapsed;
-  }
+    /**
+     * Toggles the visibility of a div element.
+     */
+    toggleDiv() {
+        this.matchMedia.showSearchDropdown = false;
+        this.isCollapsed = !this.isCollapsed;
+    }
 
-  deleteHovered(){
-    this.hovered = false;
-  }
+    /**
+     * Sets the `hovered` property to `false`.
+     */
+    deleteHovered() {
+        this.hovered = false;
+    }
 
-  openNewMessage() {
-    this.matchMedia.showSearchDropdown = false;
-    this.matchMedia.loading = true;
-    this.router.navigateByUrl('/new-message');
-  }
+    /**
+     * Opens the new message page.
+     * Hides the search dropdown and sets the loading state to true.
+     * Navigates to the '/new-message' route.
+     */
+    openNewMessage() {
+        this.matchMedia.showSearchDropdown = false;
+        this.matchMedia.loading = true;
+        this.router.navigateByUrl("/new-message");
+    }
 }
