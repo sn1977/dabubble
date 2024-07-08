ede8c25d345bb186d81f2ea06477d62108c14d9e
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-08 20:52:02 +0200

    refactor: Update emoji-picker component HTML and SCSS for better organization and formatting

--- src/app/shared/components/desktop-overlay/desktop-overlay.component.ts
+++ src/app/shared/components/desktop-overlay/desktop-overlay.component.ts
@@ -1,38 +1,47 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {AuthService} from '../../services/auth.service';
-import {ProfilCardComponent} from '../../../main-content/profile/profil-card/profil-card.component';
-import {User} from '../../../../models/user.class';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { AuthService } from "../../services/auth.service";
+import { ProfilCardComponent } from "../../../main-content/profile/profil-card/profil-card.component";
+import { User } from "../../../../models/user.class";
 
 @Component({
-  selector: 'app-desktop-overlay',
-  standalone: true,
-  imports: [],
-  templateUrl: './desktop-overlay.component.html',
-  styleUrl: './desktop-overlay.component.scss'
+    selector: "app-desktop-overlay",
+    standalone: true,
+    imports: [],
+    templateUrl: "./desktop-overlay.component.html",
+    styleUrl: "./desktop-overlay.component.scss",
 })
 export class DesktopOverlayComponent {
-  @Input() user: User | undefined
-  @Output() closeOverlay = new EventEmitter<void>(); // Hier definieren wir die closeOverlay @Output() Eigenschaft
+    @Input() user: User | undefined;
+    @Output() closeOverlay = new EventEmitter<void>(); // Hier definieren wir die closeOverlay @Output() Eigenschaft
 
-  constructor(private dialog: MatDialog, private authService: AuthService) {}
+    constructor(private dialog: MatDialog, private authService: AuthService) {}
 
-  openProfileCard(): void {
-    const dialogRef = this.dialog.open(ProfilCardComponent, {
-      minWidth: '398px',
-      minHeight: '600px',
-      panelClass: 'custom-dialog-container',
-      data: { user: this.user } // Stellen Sie sicher, dass Sie das Benutzerobjekt haben
-    });
+    /**
+     * Opens the profile card dialog.
+     *
+     * @remarks
+     * This method opens a dialog to display the profile card component.
+     *
+     * @returns void
+     */
+    openProfileCard(): void {
+        const dialogRef = this.dialog.open(ProfilCardComponent, {
+            minWidth: "398px",
+            minHeight: "600px",
+            panelClass: "custom-dialog-container",
+            data: { user: this.user }, // Stellen Sie sicher, dass Sie das Benutzerobjekt haben
+        });
 
-    dialogRef.afterClosed().subscribe(result => {
-      // console.log('Der Dialog wurde geschlossen');
-    });
+        dialogRef.afterClosed().subscribe((result) => {});
 
-    this.closeOverlay.emit(); // Hier lösen wir das closeOverlay Ereignis aus
-  }
+        this.closeOverlay.emit(); // Hier lösen wir das closeOverlay Ereignis aus
+    }
 
-  logout(): void {
-    this.authService.logout();
-  }
+    /**
+     * Logs out the user.
+     */
+    logout(): void {
+        this.authService.logout();
+    }
 }
--- src/app/shared/components/emoji-picker/emoji-picker.component.ts
+++ src/app/shared/components/emoji-picker/emoji-picker.component.ts
@@ -1,30 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { EmojiEvent } from '@ctrl/ngx-emoji-mart/ngx-emoji';
-import { PickerComponent } from '@ctrl/ngx-emoji-mart';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { EmojiEvent } from "@ctrl/ngx-emoji-mart/ngx-emoji";
+import { PickerComponent } from "@ctrl/ngx-emoji-mart";
 
 @Component({
-  selector: 'app-emoji-picker',
-  templateUrl: './emoji-picker.component.html',
-  styleUrls: ['./emoji-picker.component.scss'],
-  standalone: true,
-  imports: [
-    PickerComponent
-  ]
+    selector: "app-emoji-picker",
+    templateUrl: "./emoji-picker.component.html",
+    styleUrls: ["./emoji-picker.component.scss"],
+    standalone: true,
+    imports: [PickerComponent],
 })
 export class EmojiPickerComponent {
-  @Output() emojiSelect = new EventEmitter<string>();
+    @Output() emojiSelect = new EventEmitter<string>();
 
-  onEmojiSelect(event: EmojiEvent) {
-    if (event.emoji && event.emoji.unified) {
-      const emojiUnicode = this.convertToNative(event.emoji.unified);      
-      this.emojiSelect.emit(emojiUnicode);
+    /**
+     * Handles the selection of an emoji.
+     *
+     * @param event - The EmojiEvent object containing the selected emoji.
+     */
+    onEmojiSelect(event: EmojiEvent) {
+        if (event.emoji && event.emoji.unified) {
+            const emojiUnicode = this.convertToNative(event.emoji.unified);
+            this.emojiSelect.emit(emojiUnicode);
+        }
     }
-  }
-  convertToNative(unified: string): string {
-    return unified.split('-')
-      .map(u => String.fromCodePoint(parseInt(u, 16)))
-      .join('');
-  }
+
+    /**
+     * Converts a Unicode string to its native representation.
+     *
+     * @param unified - The Unicode string to convert.
+     * @returns The native representation of the Unicode string.
+     */
+    convertToNative(unified: string): string {
+        return unified
+            .split("-")
+            .map((u) => String.fromCodePoint(parseInt(u, 16)))
+            .join("");
+    }
 }
-
-
