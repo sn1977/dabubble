31ba60e3ff44813d992ee492dbdc4d660d3022ed
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-09 11:08:05 +0200

    refactor: Improve organization and formatting of HeaderStateService and add documentation

--- src/app/shared/services/header-state.service.ts
+++ src/app/shared/services/header-state.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class HeaderStateService {
-  private _alternativeHeader = new BehaviorSubject<boolean>(false);
-  alternativeHeader$ = this._alternativeHeader.asObservable();
+    private _alternativeHeader = new BehaviorSubject<boolean>(false);
+    alternativeHeader$ = this._alternativeHeader.asObservable();
 
-  setAlternativeHeader(value: boolean) {
-    // console.log('Setting alternativeHeader to', value);
-    this._alternativeHeader.next(value);
-  }
+    /**
+     * Sets the alternative header value.
+     *
+     * @param value - The new value for the alternative header.
+     */
+    setAlternativeHeader(value: boolean) {
+        this._alternativeHeader.next(value);
+    }
 }
