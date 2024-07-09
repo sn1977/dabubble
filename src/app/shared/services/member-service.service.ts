291826ae50523347817a8b92591bd6a3217b134c
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-09 11:11:05 +0200

    refactor: Improve organization and formatting of MatchMediaService and MemberService and add documentation

--- src/app/shared/services/member-service.service.ts
+++ src/app/shared/services/member-service.service.ts
@@ -1,26 +1,39 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class MemberService {
-  private members = new BehaviorSubject<any[]>([]);
+    private members = new BehaviorSubject<any[]>([]);
 
-  setMembers(members: any[]) {
-    this.members.next(members);
-  }
-
-  getMembers() {
-    return this.members.asObservable();
-  }
-
-  updateMemberAvatar(uid: string, newAvatarUrl: string) {
-    const members = this.members.getValue();
-    const member = members.find(member => member.uid === uid);
-    if (member) {
-      member.avatar = newAvatarUrl;
+    /**
+     * Sets the members of the service.
+     * @param {any[]} members - An array of members to set.
+     */
+    setMembers(members: any[]) {
+        this.members.next(members);
     }
-    this.setMembers(members);
-  }
+
+    /**
+     * Returns an observable of members.
+     * @returns An observable of members.
+     */
+    getMembers() {
+        return this.members.asObservable();
+    }
+
+    /**
+     * Updates the avatar of a member.
+     * @param {string} uid - The unique identifier of the member.
+     * @param {string} newAvatarUrl - The URL of the new avatar.
+     */
+    updateMemberAvatar(uid: string, newAvatarUrl: string) {
+        const members = this.members.getValue();
+        const member = members.find((member) => member.uid === uid);
+        if (member) {
+            member.avatar = newAvatarUrl;
+        }
+        this.setMembers(members);
+    }
 }
