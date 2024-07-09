e4287785688ba364689fcd62917885c35adbcbed
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-09 11:07:04 +0200

    refactor: Improve organization and formatting of DialogServiceService and add documentation

--- src/app/shared/services/dialog-service.service.ts
+++ src/app/shared/services/dialog-service.service.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { DirectMessageOverlayComponent} from '../../main-content/direct-message/direct-message-overlay/direct-message-overlay.component';
-import {User} from '../../../models/user.class';
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { DirectMessageOverlayComponent } from "../../main-content/direct-message/direct-message-overlay/direct-message-overlay.component";
+import { User } from "../../../models/user.class";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class DialogServiceService {
-  itemID: any = '';
-  user: User = new User();
+    itemID: any = "";
+    user: User = new User();
 
+    constructor(private dialog: MatDialog) {}
 
-  constructor(private dialog: MatDialog) {}
-
-  openDirectMessageDialog(userData: any, itemId: string) {
-    console.log('Übergebene Daten:', userData);
-    console.log(itemId);
-    this.dialog.open(DirectMessageOverlayComponent, {
-      minWidth: '398px',
-      minHeight: '600px',
-      panelClass: 'custom-dialog-container',
-      // data: userData
-      // data: { user: this.user,
-      //   itemId: this.itemID }
-      data: { user: userData, itemId: itemId }
-    });
-  }
+    /**
+     * Opens a direct message dialog.
+     *
+     * @param userData - The user data to pass to the dialog.
+     * @param itemId - The ID of the item to pass to the dialog.
+     */
+    openDirectMessageDialog(userData: any, itemId: string) {
+        this.dialog.open(DirectMessageOverlayComponent, {
+            minWidth: "398px",
+            minHeight: "600px",
+            panelClass: "custom-dialog-container",
+            data: { user: userData, itemId: itemId },
+        });
+    }
 }
